dbb676a0ea53c59ca7da73004379e8b5780ea0ad
villainmusic777 <villainmusicgithub@gmail.com>
2025-10-23 08:44:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,13 +41,10 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
-# ✅ API URL replaced with JSON data
-API_URL = {
-  "status": "running"
-}
+API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots4e1026') # youtube song api key
 
-VIDEO_API_URL = getenv("VIDEO_API_URL", "https://api.video.thequickearn.xyz")
-API_KEY = getenv("API_KEY", "NxGBNexGenBots4e1026")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
